18c360a9ebaf8dd261650d699b9fb64ebea20e01
DrMelt <ssxiayiz@outlook.com>
2026-03-06 01:00:12 +0800

    feat(llm): 更新LLM生成模块并添加预测输出功能
    - 修改模型加载路径和配置，使用新的模型文件
    - 更新上下文输入文本为更长的中文段落
    - 添加对forecast.txt输出文件的git忽略规则
    - 优化代码结构，改进模块导入方式
    - 修正模型实例化和设备配置逻辑

--- llm_fixed/llm_generate.py
+++ llm_fixed/llm_generate.py
@@ -1,26 +1,36 @@
 import os
+import sys
 from pathlib import Path
 import torch
 from torch.profiler import profile, ProfilerActivity, record_function
 
-from shared.module import LLM_ModelBase
+
+project_root = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(project_root))
+from shared.units import *
+from shared.module import *
+
+from llm_fixed.llm_fixed import *
 
 if __name__ == "__main__":
     current_dir = Path(__file__).resolve().parent
-    os.chdir(current_dir)
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    model: LLM_ModelBase = (
-        torch.load(f"model/model_{26*10000}.pth", weights_only=False).to(device).eval()
+    model: LLMFixedModel = (
+        torch.load(current_dir / f"model/model_{22*10000}.pth", weights_only=False)
+        .to(device)
+        .eval()
     )
     character_mapper = model.vocab_map
 
     context = torch.tensor(
-        character_mapper.encode("唐三你"),
+        character_mapper.encode(
+            "换作平时，八寻会等男人醒来后再郑重道歉，但目前时间紧迫"
+        ),
         dtype=torch.long,
         device=device,
     ).view(1, -1)
 
-    open("more.txt", "w").write(
+    open(current_dir / "forecast.txt", "w").write(
         character_mapper.decode(
             model.generate(context, max_new_tokens=2000, temperature=1.0)[0].tolist()
         )
